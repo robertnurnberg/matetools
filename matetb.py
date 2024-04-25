794f547a520c74f3f2f8a6e7dd32361f957c5d17
Robert Nurnberg <robert.nurnberg@gmx.de>
2024-04-25 15:29:15 +0200

    add parameters for mate in 77

--- matetb.py
+++ matetb.py
@@ -434,8 +434,8 @@ def fill_exclude_options(args):
         args.excludeAllowingCapture = True
         args.excludeAllowingFrom = "b3 h5 h4"
     elif epd in [
-        "8/1p4Pp/1p6/1p6/1p5p/5r2/4Kpkp/6br w - -",  # bm #77 (not yet)
-        "8/6Pp/8/8/7p/5r2/4Kpkp/6br w - -",  # bm #19 (not yet)
+        "8/6Pp/8/8/7p/5r2/4Kpkp/6br w - -",  # bm #19
+        "8/1p4Pp/1p6/1p6/1p5p/5r2/4Kpkp/6br w - -",  # bm #77
     ]:
         args.openingMoves = "g7g8q g2h3 e2f1, g7g8q f3g3 g8d5 g3f3 d5f3, g7g8q f3g3 g8d5 g2h3 d5e6 g3g4 e2f1, g7g8q f3g3 g8d5 g2h3 d5e6 h3g2 e6e4 g3f3 e4f3, g7g8q f3g3 g8d5 g2h3 d5e6 h3g2 e6e4 g2h3 e2f1"
         args.excludeFrom = "f1"
