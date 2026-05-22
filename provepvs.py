49021ac90210c061c1338fc9d3ace2c64e226664
Robert Nurnberg <28635489+robertnurnberg@users.noreply.github.com>
2026-05-22 14:47:03 +0200

    Add --backstepMateLimit

--- provepvs.py
+++ provepvs.py
@@ -208,7 +208,11 @@ class Analyser:
                 if ban_move in rootmoves[dfen]:
                     rootmoves[dfen].remove(ban_move)
 
-                limit = copy.copy(self.limit)
+                limit = (
+                    chess.engine.Limit()
+                    if args.backstepMateLimit
+                    else copy.copy(self.limit)
+                )
                 limit.mate = max(1, -pvmate - 1)
                 if rootmoves[dfen]:
                     print(
@@ -408,6 +412,11 @@ if __name__ == "__main__":
         "--goForward",
         action="store_true",
         help="Check optimality of PVs by doing a forward analysis using the limits for the root position.",
+    )
+    parser.add_argument(
+        "--backstepMateLimit",
+        action="store_true",
+        help="When improving a suboptimal defense, use a pure go-mate limit.",
     )
     parser.add_argument(
         "--logFile",
