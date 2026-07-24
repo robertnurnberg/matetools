d533c9b2949f8b5efc094982fa29a3ca929811dc
Robert Nürnberg <28635489+robertnurnberg@users.noreply.github.com>
2026-07-24 15:34:16 +0200

    generalize more scripts to new multipv format (#128)

--- diffmates.py
+++ diffmates.py
@@ -14,26 +14,29 @@ if __name__ == "__main__":
     parser.add_argument("file1")
     parser.add_argument("file2")
     args = parser.parse_args()
-    p = re.compile(r"([0-9a-zA-Z/\- ]*) bm #([0-9\-]*);")
+    p = re.compile(r"^([1-8a-zA-Z/]+ [wb] [a-zA-Z\-]+ [a-h1-8\-]+)( bm #(-?\d+);)?")
 
     d = [{}, {}]
     for idx, filename in enumerate([args.file1, args.file2]):
         with open_file(filename) as f:
             for line in f:
+                if line.startswith("#"):  # ignore comments
+                    continue
                 m = p.match(line)
                 assert m, f"error for line '{line[:-1]}' in file {filename}"
-                fen, bm = m.group(1), int(m.group(2))
+                fen = m.group(1)
+                bm = int(m.group(3)) if m.group(2) is not None else None
                 assert fen not in d[idx], f'error: duplicate FEN "{fen}" in {filename}'
                 d[idx][fen] = bm, line
 
     for fen, (bm1, line1) in d[0].items():
         bm2, line2 = d[1].get(fen, (None, None))
-        if bm2 is None:
+        if line2 is None:
             print("> " + line1 + "<\n---")
         elif bm2 != bm1:
             print("> " + line1 + "< " + line2 + "---")
 
     for fen, (_, line2) in d[1].items():
-        bm1, _ = d[0].get(fen, (None, None))
-        if bm1 is None:
+        _, line1 = d[0].get(fen, (None, None))
+        if line1 is None:
             print(">\n< " + line2 + "---")
